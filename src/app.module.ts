585612eab8320558da45f98e090e6b49830b37a0
Tung Nguyen <tungnt16092001@gmail.com>
2023-09-02 14:27:43 +0700

    Prepare to checkout feature/authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+import * as Joi from '@hapi/joi';
+import { RoleModule } from './main/role/role.module';
+import { AutoMapperModule } from './automapper/automapper.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+      validationSchema: Joi.object({
+        MYSQL_PORT: Joi.number().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+    RoleModule,
+    AutoMapperModule
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/database/database.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { databaseConnection } from './database-connection';
+
+
+@Module({
+    imports: [...databaseConnection],
+    exports: [...databaseConnection],
+})
+export class DatabaseModule {
+}
