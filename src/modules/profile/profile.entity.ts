c18da1b779025dde2f1c4874c702429a64301e3f
Gia Bảo <baosphoa0403@gmail.com>
2023-10-01 00:22:02 +0700

    set up seed

--- src/modules/profile/profile.entity.ts
+++ src/modules/profile/profile.entity.ts
@@ -25,7 +25,7 @@ export class ProfileEntity {
   @Column({ type: 'varchar', length: 255 })
   address: string;
 
-  @Column({ type: 'varchar', length: 20 })
+  @Column({ type: 'varchar', length: 100 })
   phoneNumber: string;
 
   @Column({ type: 'varchar', length: 255 })
