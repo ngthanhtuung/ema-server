2cb17b195720dbf914604123f35000deab7a78fa
Tung Nguyen <tungnt16092001@gmail.com>
2023-10-15 18:54:40 +0700

    fix : get user by id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { FirebaseMessageService } from './providers/firebase/message/firebase-message.service';
+import { Controller, InternalServerErrorException, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
+@ApiTags('TESTING API')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly firebaseMessageService: FirebaseMessageService
+  ) { }
+
+  @Get('test-notification/')
+  @Public()
+  async testNotification(
+    // @Param('deviceToken') deviceToken: string
+  ): Promise<any | undefined> {
+    try {
+      let deviceTokenArray = ['123']
+      const result = await this.firebaseMessageService.sendCustomNotification(
+        deviceTokenArray,
+        'hahahaha',
+        'test thử noti',
+        { test: 'test' }
+      )
+      console.log('Result: ', result.responses)
+    } catch (err) {
+      throw new InternalServerErrorException(err.message)
+    }
+  }
 }
