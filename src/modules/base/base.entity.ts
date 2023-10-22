b1a898dcc405c903923934b13341b8718118839c
BanhQuyDev <BanhQuyDev>
2023-10-22 16:11:27 +0700

    fix: update format createAt

--- src/modules/base/base.entity.ts
+++ src/modules/base/base.entity.ts
@@ -1,3 +1,5 @@
+import { Transform } from 'class-transformer';
+import * as moment from 'moment-timezone';
 import {
   CreateDateColumn,
   PrimaryGeneratedColumn,
@@ -9,8 +11,18 @@ export class BaseEntity {
   public id: string;
 
   @CreateDateColumn()
+  @Transform(({ value }) => {
+    return moment(value)
+      .tz('Asia/Ho_Chi_Minh')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  })
   public createdAt: Date;
 
   @UpdateDateColumn()
+  @Transform(({ value }) => {
+    return moment(value)
+      .tz('Asia/Ho_Chi_Minh')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  })
   public updatedAt: Date;
 }
