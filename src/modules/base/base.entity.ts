1dbaf3726e3aef1bf0d93348264ff595f89c9153
BanhQuyDev <BanhQuyDev>
2023-10-22 17:03:34 +0700

    feat: fix update code create time

--- src/modules/base/base.entity.ts
+++ src/modules/base/base.entity.ts
@@ -1,28 +1,20 @@
 import { Transform } from 'class-transformer';
 import * as moment from 'moment-timezone';
-import {
-  CreateDateColumn,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export class BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   public id: string;
 
-  @CreateDateColumn()
-  @Transform(({ value }) => {
-    return moment(value)
-      .tz('Asia/Ho_Chi_Minh')
-      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  @Column({
+    type: 'datetime',
+    default: moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss.SSS'),
   })
   public createdAt: Date;
 
-  @UpdateDateColumn()
-  @Transform(({ value }) => {
-    return moment(value)
-      .tz('Asia/Ho_Chi_Minh')
-      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  @Column({
+    type: 'datetime',
+    default: moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss.SSS'),
   })
   public updatedAt: Date;
 }
