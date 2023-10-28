d79288ce284bc01f1c5821514030e2aaaf5d2c5b
BanhQuyDev <BanhQuyDev>
2023-10-28 20:36:16 +0700

    fix: update codeassigne

--- src/modules/assign-task/assign-task.service.ts
+++ src/modules/assign-task/assign-task.service.ts
@@ -29,64 +29,106 @@ export class AssignTaskService extends BaseService<AssignTaskEntity> {
     return this.assignTaskRepository.createQueryBuilder('assign-tasks');
   }
 
-  async assignMemberToTask(data: AssignTaskReq, user: string): Promise<string> {
-    try {
-      const queryRunner = this.dataSource.createQueryRunner();
-      // eslint-disable-next-line prefer-const
-      let { assignee, taskID, leader } = data;
-      const oUser = JSON.parse(user);
-      if (assignee.length < 0) {
-        throw new BadRequestException(ASSIGN_ERROR_MESSAGE.NO_ASSIGNEE);
-      }
+  // async assignMemberToTask(data: AssignTaskReq, user: string): Promise<string> {
+  //   try {
+  //     const queryRunner = this.dataSource.createQueryRunner();
+  //     // eslint-disable-next-line prefer-const
+  //     let { assignee, taskID, leader } = data;
+  //     const oUser = JSON.parse(user);
+  //     if (assignee.length < 0) {
+  //       throw new BadRequestException(ASSIGN_ERROR_MESSAGE.NO_ASSIGNEE);
+  //     }
 
+  //     if (assignee.length > 0 && leader.length == 0) {
+  //       leader = assignee[0];
+  //     }
+
+  //     const callback = async (queryRunner: QueryRunner): Promise<void> => {
+  //       const taskExisted = await queryRunner.manager.findOne(TaskEntity, {
+  //         where: { id: taskID },
+  //       });
+
+  //       if (!taskExisted) {
+  //         throw new BadRequestException(TASK_ERROR_MESSAGE.TASK_NOT_FOUND);
+  //       }
+  //       const assignedExisted = await queryRunner.manager.find(
+  //         AssignTaskEntity,
+  //         {
+  //           where: { taskID },
+  //         },
+  //       );
+  //       const deleteAssignTask = assignedExisted?.map((item) => {
+  //         queryRunner.manager.delete(AssignTaskEntity, { id: item.id });
+  //       });
+  //       if (deleteAssignTask.length !== 0) {
+  //         await Promise.all(deleteAssignTask);
+  //       }
+  //       const aAssignTask = [];
+  //       assignee.map((item) => {
+  //         let isLeader = false;
+  //         if (item === leader) {
+  //           isLeader = true;
+  //         }
+  //         const oAssignTask = {
+  //           taskID,
+  //           assignee: item,
+  //           isLeader,
+  //           taskMaster: oUser.id,
+  //         };
+  //         aAssignTask.push(
+  //           queryRunner.manager.insert(AssignTaskEntity, oAssignTask),
+  //         );
+  //       });
+  //       if (aAssignTask.length !== 0) {
+  //         await Promise.all(aAssignTask);
+  //       }
+  //     };
+  //     await this.transaction(callback, queryRunner);
+  //     return 'Assign member successfully';
+  //   } catch (err) {
+  //     return err.message;
+  //   }
+  // }
+
+  async assignMemberToTask(data: AssignTaskReq, user: string): Promise<string> {
+    const { assignee, taskID } = data;
+    let { leader } = data;
+    const oUser = JSON.parse(user);
+    const queryRunner = this.dataSource.createQueryRunner();
+    const callback = async (queryRunner: QueryRunner): Promise<void> => {
+      // Check if the task exists.
+      const taskExisted = await queryRunner.manager.findOne(TaskEntity, {
+        where: { id: taskID },
+      });
       if (assignee.length > 0 && leader.length == 0) {
         leader = assignee[0];
       }
+      if (!taskExisted) {
+        throw new BadRequestException(TASK_ERROR_MESSAGE.TASK_NOT_FOUND);
+      }
 
-      const callback = async (queryRunner: QueryRunner): Promise<void> => {
-        const taskExisted = await queryRunner.manager.findOne(TaskEntity, {
-          where: { id: taskID },
-        });
+      // Delete all existing assigned tasks for the given task ID.
+      await queryRunner.manager.query(
+        `DELETE FROM assign_tasks WHERE taskID = '${taskID}'`,
+      );
 
-        if (!taskExisted) {
-          throw new BadRequestException(TASK_ERROR_MESSAGE.TASK_NOT_FOUND);
-        }
-        const assignedExisted = await queryRunner.manager.find(
-          AssignTaskEntity,
-          {
-            where: { taskID },
-          },
-        );
-        const deleteAssignTask = assignedExisted?.map((item) => {
-          queryRunner.manager.delete(AssignTaskEntity, { id: item.id });
-        });
-        if (deleteAssignTask.length !== 0) {
-          await Promise.all(deleteAssignTask);
-        }
-        const aAssignTask = [];
-        assignee.map((item) => {
-          let isLeader = false;
-          if (item === leader) {
-            isLeader = true;
-          }
-          const oAssignTask = {
-            taskID,
-            assignee: item,
-            isLeader,
+      // Insert the new assigned tasks.
+      await Promise.all(
+        assignee.map(async (assignee) => {
+          const isLeader = assignee === leader;
+          const assignTask = {
+            taskID: taskID,
+            assignee: assignee,
+            isLeader: isLeader,
             taskMaster: oUser.id,
           };
-          aAssignTask.push(
-            queryRunner.manager.insert(AssignTaskEntity, oAssignTask),
-          );
-        });
-        if (aAssignTask.length !== 0) {
-          await Promise.all(aAssignTask);
-        }
-      };
-      await this.transaction(callback, queryRunner);
-      return 'Assign member successfully';
-    } catch (err) {
-      return err.message;
-    }
+
+          return await queryRunner.manager.insert(AssignTaskEntity, assignTask);
+        }),
+      );
+    };
+    await this.transaction(callback, queryRunner);
+
+    return 'Assign member successfully';
   }
 }
