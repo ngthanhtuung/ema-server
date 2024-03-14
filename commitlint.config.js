faa39749db0d95d7767d42f6dd0013127190f185
BanhQuyDev <BanhQuyDev>
2024-03-15 05:33:54 +0700

    feat: update code

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,24 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    // Define type-enum rule as a function
     'type-enum': [
-      'build',
-      'chore',
-      'ci',
-      'docs',
-      'feat',
-      'fix',
-      'perf',
-      'refactor',
-      'revert',
-      'style',
-      'test',
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation changes
+        'style', // Changes that do not affect the meaning of the code (white-space, formatting, etc.)
+        'refactor', // Code changes that neither fix a bug nor add a feature
+        'perf', // Performance improvement
+        'test', // Adding missing tests
+        'chore', // Changes to the build process or auxiliary tools and libraries such as documentation generation
+        'build', // Changes that affect the build system or external dependencies
+        'ci', // Changes to our CI configuration files and scripts
+        'revert', // Reverts a previous commit
+        // Add more types if needed
+      ],
     ],
   },
 };
