0bb32e3959136fa42c6b73962b9d021b04236b0c
ngthanhtuung <tungnt16092001@gmail.com>
2024-03-23 15:21:47 +0700

    add more role in getAllRoles

--- src/modules/roles/roles.controller.ts
+++ src/modules/roles/roles.controller.ts
@@ -12,7 +12,7 @@ export class RolesController {
   constructor(private readonly roleService: RolesService) {}
 
   @Get()
-  @Roles(ERole.ADMIN, ERole.MANAGER)
+  @Roles(ERole.ADMIN, ERole.MANAGER, ERole.EMPLOYEE)
   async getAllRoles(): Promise<RoleEntity[] | undefined> {
     return await this.roleService.findAll();
   }
